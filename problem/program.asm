5f88015548bb4871eb072c552e904aeedbad5761
juanestebanct <78618669+juanestebanct@users.noreply.github.com>
2022-02-09 22:48:00 -0500

    prueba de si sirve o no

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,43 @@
-// COLOCO AQUÍ TU NOMBRE COMPLETO
-// COLOCA AQUÍ TU ID
-// COLOCA AQUÍ TU CORREO ELECTRÓNICO
+// juan esteban ciceri tobon 
+// 000418855
+// juan.ciceri@upb.edu.co
 (START)
-    @START
-    0;JMP
+@16384
+D=A
+@1
+M=D//posicion inicial 
+@1
+D=M
+@2 
+M=D//posicion final
+@24576
+D=M
+@2
+D;JNE //comprobamos d y si  ponemos en negro 
+@2
+D=M
+@24575
+D=D-A
+@4
+D;JGT
+@2
+A=M
+M=-1
+@2
+M=M+1
+@12
+0;JMP// comienza  vaciado llenado
+/*
+@2
+D=M 
+@24575
+D=D-A
+@4
+D;JGT
+@2
+A=M 
+M=0
+@2
+*/
+@2
+0;JMP
