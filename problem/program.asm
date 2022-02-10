804f4c50d1f0a2779a02ff26696086963bbb8442
juanestebanct <78618669+juanestebanct@users.noreply.github.com>
2022-02-10 11:17:03 -0500

    se arreglo f y añadi c

--- problem/program.asm
+++ problem/program.asm
@@ -6,38 +6,53 @@
 D=A
 @1
 M=D//posicion inicial 
+@70
+D=A
+@3
+M=D//asignamos el valor f
+@67
+D=A
+@4
+M=D//asignamos el valor c
 @1
 D=M
 @2 
 M=D//posicion final
+@3
+D=M 
 @24576
-D=M
-@2
-D;JNE //comprobamos d y si  ponemos en negro 
+D=D-M
+@35
+D;JNE //comprobamos d y si si ponemos en negro 
 @2
 D=M
 @24575
 D=D-A
-@4
+@12
 D;JGT
 @2
 A=M
 M=-1
 @2
 M=M+1
-@12
+@22
 0;JMP// comienza  vaciado llenado
-/*
-@2
+@4
 D=M 
+@24576
+D=D-M
+@12
+D;JNE //comprobamos d y si si ponemos en blanco 
+@2
+D=M
 @24575
 D=D-A
-@4
+@12
 D;JGT
 @2
-A=M 
+A=M
 M=0
 @2
-*/
-@2
+M=M+1
+@41
 0;JMP
